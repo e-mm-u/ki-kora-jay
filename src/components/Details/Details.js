13ee87e343cdf17f295392427c0a9d2392231894
e-mm-u <uh.habiba1012@gmail.com>
2022-09-29 00:21:17 +0600

    details added

---
+++ src/components/Details/Details.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Details.css'
+
+const Details = () => {
+    return (
+        <div className='details'>
+            <div className='col'>
+                <div className='first'>
+                    <h3>60</h3> 
+                    <small>kg</small>
+                </div>
+                <p>Weight</p>
+            </div>
+            <div className='col'>
+                <div className='first'>
+                    <h3>60</h3> 
+                    <small>kg</small>
+                </div>
+                <p>Weight</p>
+            </div>
+            <div className='col'>
+                <div className='first'>
+                    <h3>60</h3> 
+                    <small>kg</small>
+                </div>
+                <p>Weight</p>
+            </div>
+        </div>
+    );
+};
+
+export default Details;
