04a044e0e66cb2ecaa5eb21a8705ddd1cf752f5f
e-mm-u <uh.habiba1012@gmail.com>
2022-09-29 04:52:17 +0600

    show break time from local storage done

--- src/components/Selected/Selected.js
+++ src/components/Selected/Selected.js
@@ -2,13 +2,15 @@ import './Selected.css'
 
 const Selected = (props) => {
 
-    const {time, breakTime} = props;
+    const { time, breakTime } = props;
 
     return (
         <div>
             <h3>Selected Task Details</h3>
             <div className="selected-time">Required Time : {time} Hours</div>
-            <div className="selected-break-time">Break Time : {breakTime} Minutes</div>
+            <div className="selected-break-time">Break Time : {
+                localStorage.getItem('break-time') ? localStorage.getItem('break-time') : breakTime
+            } Minutes</div>
         </div>
     );
 };
