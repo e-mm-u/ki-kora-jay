f52686dde1eeab6fb83a30fcc0b27e418a4604df
e-mm-u <uh.habiba1012@gmail.com>
2022-09-29 14:03:46 +0600

    toast added successfully

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Toast.css'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Toast = () => {
+    const notify = () => toast("WOW! Great Lila boti. Keep it up. You are the BEST", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
     return (
-        <div>
-           <button className='toast'>Completed the tasks</button> 
+        <div >
+            <button onClick={notify} className='toast'>Completed the tasks</button>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </div>
     );
 };
